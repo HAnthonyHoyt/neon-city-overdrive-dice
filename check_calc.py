ee4133a6e3e3dfe3d21eb9b6c0316e752aae1872
Tony Hoyt <HAnthonyHoyt@gmail.com>
2022-01-01 01:00:29 -0500

    Making the full calculator multithreaded for improved performance.

--- check_calc.py
+++ check_calc.py
@@ -1,5 +1,6 @@
 import itertools
-from typing import Tuple, Set
+from multiprocessing import Pool, cpu_count
+from typing import List, Set, Tuple
 
 from lib import resolve_roll, is_success_roll, MAX_SUCCESS_PERC, MAX_ACTION_DICE, DICE_SIDES
 
@@ -11,7 +12,6 @@ def get_dice_rolls(total_dice: int) -> Set[Tuple]:
     # Also, we're not returning 'duplicate' combinations.  For example: [1, 2] and [2, 1] are the same, so we don't
     # need to check twice when we know the outcome should be same for both.
     if total_dice not in dice_rolls:
-        # dice_rolls[total_dice] = set(itertools.combinations_with_replacement(range(1, DICE_SIDES + 1), total_dice))
         dice_rolls[total_dice] = list(itertools.product(range(1, DICE_SIDES + 1), repeat=total_dice))
 
     return dice_rolls[total_dice]
@@ -33,15 +33,34 @@ def get_success_rate(action_total: int, danger_total: int) -> Tuple[float, int]:
     return percent_success, total_rolls
 
 
+def calculate_action_total(action_total: int) -> List[str]:
+    action_results: List[str] = []
+    print(f"Calculating for {action_total} action dice")
+    for danger_total in itertools.count(start=0, step=1):
+        print(f"Calcing {action_total} vs {danger_total}")
+        percent_success, total_tries = get_success_rate(action_total, danger_total)
+        results = "" if percent_success < MAX_SUCCESS_PERC else " - Good!"
+        result_string = f"{action_total} vs {danger_total} - {percent_success:2.0f}% out of {total_tries:12,} " \
+                        f"tries{results}"
+        action_results.append(result_string)
+        if percent_success < MAX_SUCCESS_PERC:
+            # We stop calculating once we're passed this value, so we can go on to the next action total
+            break
+
+    print(f"Done calculating for {action_total} action dice")
+    return action_results
+
+
 def main():
-    for action_total in range(1, MAX_ACTION_DICE + 1):
-        for danger_total in itertools.count(start=0, step=1):
-            percent_success, total_tries = get_success_rate(action_total, danger_total)
-            results = "" if percent_success < MAX_SUCCESS_PERC else " - Good!"
-            print(f"{action_total} vs {danger_total} - {percent_success:2.0f}% out of {total_tries:12,} tries{results}")
-            if percent_success < MAX_SUCCESS_PERC:
-                # We stop calculating once we're passed this value, so we can go on to the next action total
-                break
+    print(f"Starting calculations, attempting all combinations for 1 to {MAX_ACTION_DICE} action dice")
+    print("Results will take a long time to generate, please be patient")
+    with Pool(cpu_count() - 1) as proc_pool:
+        dice_results = proc_pool.map(calculate_action_total, range(1, MAX_ACTION_DICE + 1))
+        proc_pool.close()
+        proc_pool.join()
+
+    for dice_result in dice_results:
+        print('\n'.join(dice_result))
 
 
 if __name__ == '__main__':
